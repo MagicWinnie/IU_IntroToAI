1bc39002edb65ecba6867e5340c002af01f52c40
MagicWinnie <magicwinnie@yandex.ru>
2023-10-14 00:25:54 +0300

    Added moore with ears

--- Assignment1/neighbourhoods.py
+++ Assignment1/neighbourhoods.py
@@ -7,3 +7,7 @@ def in_moore(point: Tuple[int, int], center: Tuple[int, int], r: int = 1) -> boo
 
 def in_von_neumann(point: Tuple[int, int], center: Tuple[int, int], r: int) -> bool:
     return abs(point[0] - center[0]) + abs(point[1] - center[1]) <= r
+
+
+def in_moore_with_ears(point: Tuple[int, int], center: Tuple[int, int], r: int = 1) -> bool:
+    return in_moore(point, center, r) or (abs(point[0] - center[0]) == r + 1 and abs(point[1] - center[1]) == r + 1)
