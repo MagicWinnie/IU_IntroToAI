cbc94ea17d52895e297d172dabf18cbfd8fc8840
MagicWinnie <magicwinnie@yandex.ru>
2023-10-28 15:25:12 +0300

    Added independent run_tests.py

--- Assignment1/run_tests.py
+++ Assignment1/run_tests.py
@@ -2,9 +2,88 @@ import os
 import glob
 import time
 import subprocess
+from enum import Enum
 from random import randint
+from typing import Tuple, List
+from dataclasses import dataclass
 from argparse import ArgumentParser, Namespace
-from map import Map, Entity
+
+
+N = 9  # size of the map (NxN)
+DASH_LENGTH = 50
+
+
+class Entity(str, Enum):
+    HULK = "H"
+    INFINITY_STONE = "I"
+    THOR = "T"
+    CAPTAIN_MARVEL = "M"
+    SHIELD = "S"
+    PERCEPTION = "P"
+    EMPTY = "."
+    PATH = "*"
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return self.value
+
+
+@dataclass
+class Cell:
+    x: int
+    y: int
+
+    def __add__(self, other: "Cell") -> "Cell":
+        return Cell(self.x + other.x, self.y + other.y)
+
+    def manhattan(self, other: "Cell") -> int:
+        return abs(self.x - other.x) + abs(self.y - other.y)
+
+
+def in_moore(point: Cell, center: Cell, r: int = 1) -> bool:
+    return abs(point.x - center.x) <= r and abs(point.y - center.y) <= r
+
+
+def in_moore_with_ears(point: Cell, center: Cell, r: int = 1) -> bool:
+    return in_moore(point, center, r) or (abs(point.x - center.x) == r + 1 and abs(point.y - center.y) == r + 1)
+
+
+def in_von_neumann(point: Cell, center: Cell, r: int) -> bool:
+    return point.manhattan(center) <= r
+
+
+def remove_perception(map_: List[List[Entity]]):
+    for i in range(N):
+        for j in range(N):
+            if map_[i][j] == Entity.PERCEPTION:
+                map_[i][j] = Entity.EMPTY
+
+
+def get_surroundings(map_: List[List[Entity]], perception: int, cell: Cell) -> List[Tuple[Cell, Entity]]:
+    output: List[Tuple[Cell, Entity]] = []
+    for i in range(N):
+        for j in range(N):
+            if map_[i][j] == Entity.EMPTY:
+                continue
+            if perception == 1:
+                if in_moore(Cell(i, j), cell):
+                    output.append((Cell(i, j), map_[i][j]))
+            elif perception == 2:
+                if in_moore_with_ears(Cell(i, j), cell):
+                    output.append((Cell(i, j), map_[i][j]))
+    return output
+
+
+def illegal_move(msg: str, curr: Cell, future: Cell):
+    print(f"[ERROR] {msg}:")
+    print("-" * DASH_LENGTH)
+    print("Tried to move to cell:")
+    print(future)
+    print("From cell:")
+    print(curr)
+    print("-" * DASH_LENGTH)
 
 
 def parse_args() -> Namespace:
@@ -40,9 +119,6 @@ def parse_args() -> Namespace:
     return parser.parse_args()
 
 
-DASH_LENGTH = 50
-
-
 def main():
     args = parse_args()
 
@@ -56,13 +132,22 @@ def main():
     for test in sorted(tests):
         print("-" * (DASH_LENGTH // 2) + test + "-" * (DASH_LENGTH // 2))
 
-        map_ = Map()
-        map_.load(test)
-        print(map_)
+        map_ = [[Entity.EMPTY for _ in range(N)] for _ in range(N)]
+        infinity_stone = Cell(-1, -1)
+        captain_marvel = Cell(-1, -1)
+        with open(test, "r") as test_fp:
+            for i, line in enumerate(test_fp):
+                for j, e in enumerate(line.split()):
+                    entity = Entity(e)
+                    if entity == Entity.INFINITY_STONE:
+                        infinity_stone = Cell(i, j)
+                    elif entity == Entity.CAPTAIN_MARVEL:
+                        captain_marvel = Cell(i, j)
+                    map_[i][j] = entity
+                print(" ".join(map_[i]))
 
-        prev_cell = (0, 0)
+        prev_cell = Cell(0, 0)
         variant_number = args.variant if args.variant in (1, 2) else randint(1, 2)
-        infinity_stone = map_.get_location(Entity.INFINITY_STONE)
 
         print("Variant number:", variant_number)
 
@@ -81,7 +166,7 @@ def main():
         start_time = time.time()
 
         proc.stdin.write(f"{variant_number}\n".encode("ASCII"))
-        proc.stdin.write(f"{infinity_stone[0]} {infinity_stone[1]}\n".encode("ASCII"))
+        proc.stdin.write(f"{infinity_stone.x} {infinity_stone.y}\n".encode("ASCII"))
         proc.stdin.flush()
 
         while True:
@@ -96,47 +181,37 @@ def main():
                 exit(1)
             elif output[0] == "m":
                 _, x, y = output.split()
-                move_cell = (int(x), int(y))
-                if abs(move_cell[0] - prev_cell[0]) + abs(move_cell[1] - prev_cell[1]) > 1:
-                    print(f"[ERROR] Can't teleport:")
-                    print("-" * DASH_LENGTH)
-                    print("Tried to move to cell:")
-                    print(move_cell)
-                    print("From cell:")
-                    print(prev_cell)
-                    print("-" * DASH_LENGTH)
+                move_cell = Cell(int(x), int(y))
+                if move_cell.manhattan(prev_cell) > 1:
+                    illegal_move("Can't teleport", prev_cell, move_cell)
                     fp.close()
                     exit(1)
-                elif map_.get(move_cell) in (Entity.CAPTAIN_MARVEL, Entity.HULK, Entity.THOR):
-                    print(f"[ERROR] Can't move into a cell with Avengers:")
-                    print("-" * DASH_LENGTH)
-                    print("Tried to move to cell:")
-                    print(move_cell)
-                    print("From cell:")
-                    print(prev_cell)
-                    print("-" * DASH_LENGTH)
+                elif map_[move_cell.x][move_cell.y] in (Entity.CAPTAIN_MARVEL, Entity.HULK, Entity.THOR):
+                    illegal_move("Can't move into a cell with Avengers", prev_cell, move_cell)
                     fp.close()
                     exit(1)
-                elif map_.get(move_cell) == Entity.PERCEPTION:
-                    print(f"[ERROR] Can't move into perception zone of Avengers:")
-                    print("-" * DASH_LENGTH)
-                    print("Tried to move to cell:")
-                    print(move_cell)
-                    print("From cell:")
-                    print(prev_cell)
-                    print("-" * DASH_LENGTH)
+                elif map_[move_cell.x][move_cell.y] == Entity.PERCEPTION:
+                    illegal_move("Can't move into perception zone of Avengers", prev_cell, move_cell)
                     fp.close()
                     exit(1)
                 else:
                     prev_cell = move_cell
-                if map_.get(move_cell) == Entity.SHIELD:
-                    map_.remove_perception(Entity.HULK)
-                    map_.remove_perception(Entity.THOR)
+                if map_[move_cell.x][move_cell.y] == Entity.SHIELD:
+                    for i in range(N):
+                        for j in range(N):
+                            if map_[i][j] == Entity.PERCEPTION:
+                                map_[i][j] = Entity.EMPTY
+                    for i in range(N):
+                        for j in range(N):
+                            if map_[i][j] != Entity.EMPTY:
+                                continue
+                            if in_von_neumann(Cell(i, j), captain_marvel, 2):
+                                map_[i][j] = Entity.PERCEPTION
 
-                surroundings = map_.get_surroundings(variant_number, move_cell)
+                surroundings = get_surroundings(map_, variant_number, move_cell)
                 proc.stdin.write(f"{len(surroundings)}\n".encode("ASCII"))
                 for cell, entity in surroundings:
-                    proc.stdin.write(f"{cell[0]} {cell[1]} {entity.value}\n".encode("ASCII"))
+                    proc.stdin.write(f"{cell.x} {cell.y} {entity.value}\n".encode("ASCII"))
                 proc.stdin.flush()
             elif output[0] == "e":
                 print("[INFO] Output:")
