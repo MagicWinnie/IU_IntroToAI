5bd0922d22078e6ee09a7ded2f5d9fd101b5cd81
MagicWinnie <magicwinnie@yandex.ru>
2023-10-29 18:32:15 +0300

    Removed my classes from run_tests

--- Assignment1/run_tests.py
+++ Assignment1/run_tests.py
@@ -3,82 +3,57 @@ import re
 import glob
 import time
 import subprocess
-from enum import Enum
 from random import randint
 from typing import Tuple, List
-from dataclasses import dataclass
 from argparse import ArgumentParser, Namespace
 
 
 N = 9  # size of the map (NxN)
 DASH_LENGTH = 50
-FILE_PATTERN = re.compile(r'.*?(\d+).*?')
+FILE_PATTERN = re.compile(r".*?(\d+).*?")
 
 
-class Entity(str, Enum):
-    HULK = "H"
-    INFINITY_STONE = "I"
-    THOR = "T"
-    CAPTAIN_MARVEL = "M"
-    SHIELD = "S"
-    PERCEPTION = "P"
-    EMPTY = "."
-    PATH = "*"
-
-    def __str__(self):
-        return self.value
-
-    def __repr__(self):
-        return self.value
+def m_dist(this, other) -> int:
+    return abs(this[0] - other[0]) + abs(this[1] - other[1])
 
 
-@dataclass
-class Cell:
-    x: int
-    y: int
-
-    def __add__(self, other: "Cell") -> "Cell":
-        return Cell(self.x + other.x, self.y + other.y)
-
-    def manhattan(self, other: "Cell") -> int:
-        return abs(self.x - other.x) + abs(self.y - other.y)
+def moore_perception_zone(point, center, r=1) -> bool:
+    return abs(point[0] - center[0]) <= r and abs(point[1] - center[1]) <= r
 
 
-def in_moore(point: Cell, center: Cell, r: int = 1) -> bool:
-    return abs(point.x - center.x) <= r and abs(point.y - center.y) <= r
+def moore_corner_perception_zone(point, center, r=1) -> bool:
+    return moore_perception_zone(point, center, r) or (
+        abs(point[0] - center[0]) == r + 1 and abs(point[1] - center[1]) == r + 1
+    )
 
 
-def in_moore_with_ears(point: Cell, center: Cell, r: int = 1) -> bool:
-    return in_moore(point, center, r) or (abs(point.x - center.x) == r + 1 and abs(point.y - center.y) == r + 1)
+def vonneumann_perception_zone(point, center, r) -> bool:
+    return m_dist(point, center) <= r
 
 
-def in_von_neumann(point: Cell, center: Cell, r: int) -> bool:
-    return point.manhattan(center) <= r
-
-
-def remove_perception(map_: List[List[Entity]]):
+def remove_perception(map_):
     for i in range(N):
         for j in range(N):
-            if map_[i][j] == Entity.PERCEPTION:
-                map_[i][j] = Entity.EMPTY
+            if map_[i][j] == "P":
+                map_[i][j] = "."
 
 
-def get_surroundings(map_: List[List[Entity]], perception: int, cell: Cell) -> List[Tuple[Cell, Entity]]:
-    output: List[Tuple[Cell, Entity]] = []
+def get_surroundings(map_, perception, cell) -> List[Tuple[Tuple[int, int], str]]:
+    output = []
     for i in range(N):
         for j in range(N):
-            if map_[i][j] == Entity.EMPTY:
+            if map_[i][j] == ".":
                 continue
             if perception == 1:
-                if in_moore(Cell(i, j), cell):
-                    output.append((Cell(i, j), map_[i][j]))
+                if moore_perception_zone((i, j), cell):
+                    output.append(((i, j), map_[i][j]))
             elif perception == 2:
-                if in_moore_with_ears(Cell(i, j), cell):
-                    output.append((Cell(i, j), map_[i][j]))
+                if moore_corner_perception_zone((i, j), cell):
+                    output.append(((i, j), map_[i][j]))
     return output
 
 
-def illegal_move(msg: str, curr: Cell, future: Cell):
+def illegal_move(msg, curr, future):
     print(f"[ERROR] {msg}:")
     print("-" * DASH_LENGTH)
     print("Tried to move to cell:")
@@ -91,7 +66,7 @@ def illegal_move(msg: str, curr: Cell, future: Cell):
 def get_order(file: str):
     match = FILE_PATTERN.match(os.path.splitext(os.path.basename(file))[0])
     if not match:
-        return float('inf')
+        return float("inf")
     return int(match.groups()[0])
 
 
@@ -141,21 +116,20 @@ def main():
     for test in sorted(tests, key=get_order):
         print("-" * (DASH_LENGTH // 2) + test + "-" * (DASH_LENGTH // 2))
 
-        map_ = [[Entity.EMPTY for _ in range(N)] for _ in range(N)]
-        infinity_stone = Cell(-1, -1)
-        captain_marvel = Cell(-1, -1)
+        map_ = [["." for _ in range(N)] for _ in range(N)]
+        infinity_stone = (-1, -1)
+        captain_marvel = (-1, -1)
         with open(test, "r") as test_fp:
             for i, line in enumerate(test_fp):
-                for j, e in enumerate(line.split()):
-                    entity = Entity(e)
-                    if entity == Entity.INFINITY_STONE:
-                        infinity_stone = Cell(i, j)
-                    elif entity == Entity.CAPTAIN_MARVEL:
-                        captain_marvel = Cell(i, j)
+                for j, entity in enumerate(line.split()):
+                    if entity == "I":
+                        infinity_stone = (i, j)
+                    elif entity == "M":
+                        captain_marvel = (i, j)
                     map_[i][j] = entity
                 print(" ".join(map_[i]))
 
-        prev_cell = Cell(0, 0)
+        prev_cell = (0, 0)
         variant_number = args.variant if args.variant in (1, 2) else randint(1, 2)
 
         print("Variant number:", variant_number)
@@ -175,7 +149,7 @@ def main():
         start_time = time.time()
 
         proc.stdin.write(f"{variant_number}\n".encode("ASCII"))
-        proc.stdin.write(f"{infinity_stone.x} {infinity_stone.y}\n".encode("ASCII"))
+        proc.stdin.write(f"{infinity_stone[0]} {infinity_stone[1]}\n".encode("ASCII"))
         proc.stdin.flush()
 
         while True:
@@ -190,37 +164,37 @@ def main():
                 exit(1)
             elif output[0] == "m":
                 _, x, y = output.split()
-                move_cell = Cell(int(x), int(y))
-                if move_cell.manhattan(prev_cell) > 1:
+                move_cell = (int(x), int(y))
+                if m_dist(move_cell, prev_cell) > 1:
                     illegal_move("Can't teleport", prev_cell, move_cell)
                     fp.close()
                     exit(1)
-                elif map_[move_cell.x][move_cell.y] in (Entity.CAPTAIN_MARVEL, Entity.HULK, Entity.THOR):
+                elif map_[move_cell[0]][move_cell[1]] in ("M", "H", "T"):
                     illegal_move("Can't move into a cell with Avengers", prev_cell, move_cell)
                     fp.close()
                     exit(1)
-                elif map_[move_cell.x][move_cell.y] == Entity.PERCEPTION:
+                elif map_[move_cell[0]][move_cell[1]] == "P":
                     illegal_move("Can't move into perception zone of Avengers", prev_cell, move_cell)
                     fp.close()
                     exit(1)
                 else:
                     prev_cell = move_cell
-                if map_[move_cell.x][move_cell.y] == Entity.SHIELD:
+                if map_[move_cell[0]][move_cell[1]] == "S":
                     for i in range(N):
                         for j in range(N):
-                            if map_[i][j] == Entity.PERCEPTION:
-                                map_[i][j] = Entity.EMPTY
+                            if map_[i][j] == "P":
+                                map_[i][j] = "."
                     for i in range(N):
                         for j in range(N):
-                            if map_[i][j] != Entity.EMPTY:
+                            if map_[i][j] != ".":
                                 continue
-                            if in_von_neumann(Cell(i, j), captain_marvel, 2):
-                                map_[i][j] = Entity.PERCEPTION
+                            if vonneumann_perception_zone((i, j), captain_marvel, 2):
+                                map_[i][j] = "P"
 
                 surroundings = get_surroundings(map_, variant_number, move_cell)
                 proc.stdin.write(f"{len(surroundings)}\n".encode("ASCII"))
                 for cell, entity in surroundings:
-                    proc.stdin.write(f"{cell.x} {cell.y} {entity.value}\n".encode("ASCII"))
+                    proc.stdin.write(f"{cell[0]} {cell[1]} {entity}\n".encode("ASCII"))
                 proc.stdin.flush()
             elif output[0] == "e":
                 print("Output:", output)
