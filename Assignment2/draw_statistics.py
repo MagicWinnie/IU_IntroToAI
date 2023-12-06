9bd44924799ccb0d54bc8b64d5bec5297d8b1323
MagicWinnie <magicwinnie@yandex.ru>
2023-12-06 23:04:01 +0300

    Modified plots

--- Assignment2/draw_statistics.py
+++ Assignment2/draw_statistics.py
@@ -28,19 +28,22 @@ for word_num in word_nums:
     fitness_mean.append(filtered["fitness"].mean())
     fitness_max.append(filtered["fitness"].max())
 
-titles = iter(
-    [
-        "Time (mean), s",
-        "Time (max), s",
-        "Generation (mean), #",
-        "Generation (max), #",
-        "Fitness (mean), #",
-        "Fitness (max), #",
-    ]
-)
-for y in time_mean, time_max, generation_mean, generation_max, fitness_mean, fitness_max:
-    title = next(titles)
-    plt.plot(word_nums, y, marker='o')
-    plt.title(title)
-    plt.savefig(f"figures/{title}.png")
+titles = [
+    "Time (mean), s",
+    "Time (max), s",
+    "Generation (mean), #",
+    "Generation (max), #",
+    "Fitness (mean), #",
+    "Fitness (max), #",
+]
+ys = time_mean, time_max, generation_mean, generation_max, fitness_mean, fitness_max
+for i in range(0, len(ys), 2):
+    for j in 0, 1:
+        plt.plot(word_nums, ys[i + j], marker='o', label=titles[i + j])
+        plt.title(titles[i + j].split(", ")[0])
+        plt.xlabel("# of words")
+        plt.ylabel(", ".join(titles[i + j].split()[::2]))
+    plt.subplots_adjust(right=0.7)
+    plt.legend(bbox_to_anchor=(1.04, 0.5), loc="center left", borderaxespad=0)
+    plt.savefig(f"figures/{titles[i].split()[0]}.png")
     plt.show()
